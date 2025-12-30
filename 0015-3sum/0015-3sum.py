54c555c3aaf3f50c079027413ad9f1759cef0e8a
Ujjval Rajput <101949411+UjjvalRajput@users.noreply.github.com>
2025-12-30 02:20:44 -0500

    Time: 594 ms (45.57%), Space: 20.1 MB (94.82%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,36 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res = []
-        n = len(nums)
-
-        for left in range(n - 2):
-            # skip duplicate fixed values
+        acc = []
+        for left in range(len(nums) - 2): # index
             if left > 0 and nums[left] == nums[left - 1]:
                 continue
-
             middle = left + 1
-            right = n - 1
+            right = len(nums) - 1
 
             while middle < right:
-                s = nums[left] + nums[middle] + nums[right]
-
-                if s == 0:
-                    res.append([nums[left], nums[middle], nums[right]])
-
-                    # skip duplicates for middle
+                total = nums[left] + nums[middle] + nums[right]
+                if total == 0:
+                    acc.append([nums[left], nums[middle], nums[right]])
                     while middle < right and nums[middle] == nums[middle + 1]:
                         middle += 1
-                    # skip duplicates for right
                     while middle < right and nums[right] == nums[right - 1]:
                         right -= 1
-
+                    
                     middle += 1
                     right -= 1
-
-                elif s < 0:
+                elif total < 0:
                     middle += 1
                 else:
                     right -= 1
-
-        return res
+        return acc
