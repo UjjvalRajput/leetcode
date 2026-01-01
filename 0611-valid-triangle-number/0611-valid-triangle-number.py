a85a716cd2485e4145ba0d99a2bab809c58994e6
Ujjval Rajput <101949411+UjjvalRajput@users.noreply.github.com>
2026-01-01 00:35:03 -0500

    Time: 419 ms (84.11%), Space: 17.4 MB (94.16%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.py
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -2,13 +2,13 @@ class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums.sort()
         acc = 0
-        for i in range(len(nums) - 1, 1, -1):
+        for i in range(len(nums) - 1, 1, -1): # ignore the first two values as i because those will be for l and r
             left = 0
             right = i - 1
             while left < right:
                 if nums[left] + nums[right] > nums[i]:
-                    acc += right - left
-                    right -= 1
+                    acc += right - left # add all values between l and r
+                    right -= 1 # try with a different r
                 else:
-                    left += 1
+                    left += 1 # smallest value is too small try a bigger value instead.
         return acc
