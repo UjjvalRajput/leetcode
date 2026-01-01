140728c7908b64faf68ba384c33cac4081761a3e
Ujjval Rajput <101949411+UjjvalRajput@users.noreply.github.com>
2026-01-01 02:32:36 -0500

    Time: 9 ms (74.14%), Space: 18.9 MB (99.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,17 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left = 0
-        right = len(height) - 1
-        left_max = height[0]
-        right_max = height[-1]
-        acc = 0
+        left = 0 # tracking height from left
+        right = len(height) - 1 # tracking height from right
+        left_max = height[0] # tracking max height seen from left
+        right_max = height[-1] # tracking max height seen from right
+        acc = 0 # tracking water total
         while left < right:
+            # find which side is shorter and track that side as it is guaranteed to hold that much water from the other side
             if right_max < left_max:
-                right -= 1
-                if height[right] < right_max:
+                right -= 1 # go to next element find height that needs to be added to acc
+                if height[right] < right_max: # if its smaller height than max seen so far, it can be safely added
                     acc += right_max - height[right]
-                else:
+                else: # otherwise no water gets added and max gets updated
                     right_max = height[right]
+            # repeat same but for left side
             else:
                 left += 1
                 if height[left] < left_max:
