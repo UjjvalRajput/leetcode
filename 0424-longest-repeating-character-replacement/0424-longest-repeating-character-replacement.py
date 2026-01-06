c60daf72af5a5669a5bf3a0c3007d0e3f14ac641
Ujjval Rajput <101949411+UjjvalRajput@users.noreply.github.com>
2026-01-06 01:26:06 -0500

    Time: 113 ms (75.62%), Space: 19.9 MB (16.47%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,10 +3,10 @@ class Solution:
         count = {} # to count max character appearance in window
         left = 0 # left pointer
         acc = 0 # results (substring with largest length)
-        for right in range(len(s)):
-            count[s[right]] = count.get(s[right], 0) + 1
-            if (right - left + 1) - max(count.values()) > k:
-                count[s[left]] -= 1
-                left += 1
-            acc = max(acc, right - left + 1)
-        return acc
+        for right in range(len(s)): # keep going right
+            count[s[right]] = count.get(s[right], 0) + 1 # update count of encountered character
+            if (right - left + 1) - max(count.values()) > k: # if window length - max char not to be changed = number of characters that are to be changed in the window is > no. of replacements allowed, the window becomes invalid. So, increment left to make window valid
+                count[s[left]] -= 1 # decrement count of left as not including it anymore
+                left += 1 # increment left pointer
+            acc = max(acc, right - left + 1) # now calculate max results again after ensuring window is valid
+        return acc # return max result
