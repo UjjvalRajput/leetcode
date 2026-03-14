5c6d3618d916873e6ba36c370a4c33e3e23f5c2e
Ujjval Rajput <101949411+UjjvalRajput@users.noreply.github.com>
2026-03-14 01:35:28 -0400

    Time: 0 ms (100%), Space: 19.1 MB (99.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,21 @@
 class Solution:
-    def climbStairs(self, n: int, seen=None) -> int:
-        if seen is None:
-            seen = {1:1, 2:2}
+    def climbStairs(self, n: int) -> int:
+        # for recursion - memoization, we can add another parameter seen to the function above:
+        # if seen is None:
+        #     seen = {1:1, 2:2}
+        # if n <= 2:
+        #     return n
+
+        # elif n in seen:
+        #     return seen[n]
+
+        # seen[n] = self.climbStairs(n-1, seen) + self.climbStairs(n-2, seen)
+        # return seen[n]
         if n <= 2:
             return n
-
-        elif n in seen:
-            return seen[n]
-
-        seen[n] = self.climbStairs(n-1, seen) + self.climbStairs(n-2, seen)
-        return seen[n]
+        
+        dp = [0] * (n+1)
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
